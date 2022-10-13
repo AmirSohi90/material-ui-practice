d2e428cc1e81be0bfacffadc2ce86522adcc2a05
amirsohi90 <amirasohi@gmail.com>
2022-10-13 22:59:45 +0100

    finished typography lesson

--- src/Pages/Create.tsx
+++ src/Pages/Create.tsx
@@ -1,3 +1,10 @@
 import React from 'react';
+import Typography from '@mui/material/Typography';
 
-export const Create: React.FC = () => <div>Create page</div>;
+export const Create: React.FC = () => (
+  <div>
+    <Typography variant='h6' component='h2' gutterBottom color='textSecondary'>
+      Create a new note
+    </Typography>
+  </div>
+);
