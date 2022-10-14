786458ff7a523cca4e870ddd17bb82c213a8de86
Amir Sohi <amir.sohi@cazoo.co.uk>
2022-10-14 16:41:09 +0100

    custom styles done

--- src/Pages/Create.tsx
+++ src/Pages/Create.tsx
@@ -4,11 +4,28 @@ import Button from '@mui/material/Button'
 import Container from '@mui/material/Container' //applies padding and margin to content
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
-export const Create: React.FC = () => (
-  <Container>
-    <Typography variant='h6' component='h2' gutterBottom color='textSecondary'>
-      Create a new note
-    </Typography>
-      <Button endIcon={<KeyboardArrowRightIcon />} type='submit' color='secondary' variant='contained' onClick={() => console.log('Thanks for clicking')}>Submit</Button>
-  </Container>
-);
+const styles = { // takes an argument where you can make your css classes and rules
+    btn: {
+        fontSize: 60,
+        backgroundColor: 'violet',
+        '&:hover': {
+            backgroundColor: 'black'
+        }
+    },
+    title: {
+        textDecoration: 'underline',
+        marginBottom: '20px'
+    }
+}
+
+export const Create: React.FC = () => {
+   return (
+        <Container>
+            <Typography sx={styles.title} variant='h6' component='h2' gutterBottom color='textSecondary'>
+                Create a new note
+            </Typography>
+            <Button sx={styles.btn} endIcon={<KeyboardArrowRightIcon/>} type='submit' color='secondary' variant='contained'
+                    onClick={() => console.log('Thanks for clicking')}>Submit</Button>
+        </Container>
+    )
+};
