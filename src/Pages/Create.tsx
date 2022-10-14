fb12efc1ede86bfed66b8c70c0b2e3a4cbf3c267
Amir Sohi <amir.sohi@cazoo.co.uk>
2022-10-14 16:17:15 +0100

    icons lesson done

--- src/Pages/Create.tsx
+++ src/Pages/Create.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button'
 import Container from '@mui/material/Container' //applies padding and margin to content
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 export const Create: React.FC = () => (
   <Container>
     <Typography variant='h6' component='h2' gutterBottom color='textSecondary'>
       Create a new note
     </Typography>
-      <Button type='submit' color='secondary' variant='contained' onClick={() => console.log('Thanks for clicking')}>Submit</Button>
+      <Button endIcon={<KeyboardArrowRightIcon />} type='submit' color='secondary' variant='contained' onClick={() => console.log('Thanks for clicking')}>Submit</Button>
   </Container>
 );
